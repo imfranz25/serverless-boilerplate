e83c71f184b7418d7a3e859a1c6c692b481c5795
Francis Ong <francis.ong@stratpoint.com>
2023-04-15 23:12:18 +0800

    feat(src/controllers/product.controller.ts): add sample validation

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,10 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
+import { productSchema } from '../schemas';
 
 /**
  * @route POSt /product/add
  */
 const addProduct = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    productSchema.validate(req.body);
+    console.log(req.body);
     res.status(200).json({ message: 'Product successfully added' });
   } catch (error) {
     next(error);
