ceb56b33e1d4bbaff41592b4954833d38eb8c77e
Francis Ong <francis.ong@stratpoint.com>
2023-04-15 23:14:07 +0800

    feat(src/index.ts): modify config + seperate error controllers

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,29 @@
-import { config } from 'dotenv';
 import express, { Application } from 'express';
 import serverless from 'serverless-http';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
+import helmet from 'helmet';
+import appConfig from './infra/configs';
 
-/* Routes */
+/* Controllers & Routes */
+import { errorController } from './controllers';
 import { productRouter } from './routes';
 
+/* App + dotEnv init */
 const app: Application = express();
-config();
 
-/* Express middleware */
+/* Express middleware(s) */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
+/* Set various HTTP headers using Helmet */
+if (appConfig.ENVIRONMENT !== 'development') {
+  app.use(helmet());
+}
+
+/* Root route */
 app.get('/', (req, res) => {
   return res.status(200).json({
     message: 'Head Start Commerce',
@@ -25,20 +33,23 @@ app.get('/', (req, res) => {
 /* Use Routes */
 app.use('/products', productRouter);
 
-/* Error Route */
-app.use((req, res) => {
+/* 404 Error Route */
+app.use('*', (req, res) => {
   return res.status(404).json({
     error: 'Not Found',
   });
 });
 
-if (process.env.ENV === 'development') {
-  const PORT = process.env.PORT || 3000;
+/* Server Error Route */
+app.use(errorController.serverError);
 
-  app.listen(PORT, () => {
+/* Run in a node server */
+if (appConfig.ENVIRONMENT === 'development') {
+  app.listen(appConfig.PORT, () => {
     /* eslint-disable-next-line no-console */
-    console.log(`Server running @ PORT ${PORT}`);
+    console.log(`Server running @ PORT ${appConfig.PORT}`);
   });
 }
 
+/* For serverless deployment */
 export const handler = serverless(app);
